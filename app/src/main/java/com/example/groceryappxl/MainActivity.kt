5f10445b879975db3ca0576949751397024748ba
Evalenc6 <117995655+Evalenc6@users.noreply.github.com>
2023-04-22 14:50:32 -0400

    New Comment Test

--- app/src/main/java/com/example/groceryappxl/MainActivity.kt
+++ app/src/main/java/com/example/groceryappxl/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
+        //new comment
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
