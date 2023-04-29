d18502b233ce47d8f6adc2f2033c13be3413d32c
Ella Goode <ellagoode9@gmail.com>
2023-04-28 22:27:17 -0400

    Added JSON

--- app/src/main/java/com/example/groceryappxl/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/groceryappxl/ui/home/HomeFragment.kt
@@ -1,17 +1,30 @@
 package com.example.groceryappxl.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestHeaders
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.example.groceryappxl.databinding.FragmentHomeBinding
+import okhttp3.Headers
+import org.json.JSONArray
 
 class HomeFragment : Fragment() {
 
+    private lateinit var recipeNameList: MutableList<String>
+    private lateinit var recipeImageList: MutableList<String>
+    private lateinit var preferencesList: MutableList<String>
+    private lateinit var ingredientStorage: MutableList<List<String>>
     private var _binding: FragmentHomeBinding? = null
+    private val apiKey = "?apiKey=f46430a89c5f4a9ba1a2dd27290b9765"
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,16 +41,104 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val searchView: SearchView = binding.searchBar
+        recipeImageList = mutableListOf()
+        recipeNameList = mutableListOf()
+        preferencesList = mutableListOf()
+        ingredientStorage = mutableListOf()
+        val url = "https://api.spoonacular.com/recipes/complexSearch"
 
-        //val textView: TextView = binding.textHome
-        //homeViewModel.text.observe(viewLifecycleOwner) {
-            //textView.text = it
-        //}
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                getSpoonAcular(query, url)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Handle search query text change
+                return true
+            }
+        })
+
         return root
     }
 
+    private fun getSpoonAcular(query: String?, url: String){
+        val client = AsyncHttpClient()
+        val fullUrl = "$url$apiKey&query=$query&instructionsRequired=true&number=1&addRecipeNutrition=false&fillIngredients=true"
+        val params =  RequestParams();
+
+        val requestHeaders = RequestHeaders()
+        requestHeaders["Content-Type"] = "application/json"
+
+        //requestHeaders, params
+        client[fullUrl, requestHeaders, params, object:
+            JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.d("JSON", "Request Failed $response")
+
+            }
+
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers?,
+                json: JsonHttpResponseHandler.JSON
+            ) {
+
+                Log.d("Status Code", "$statusCode")
+                if (recipeNameList.isNotEmpty()) {
+                    recipeNameList.clear()
+                    recipeImageList.clear()
+                    ingredientStorage.clear()
+                }
+
+                Log.d("JSON Response", "$json")
+                var ingredientsArray: JSONArray
+                val recipeJSON = json.jsonObject.getJSONArray("results")
+
+
+                //get all the ingredients
+                ingredientsArray = recipeJSON.getJSONObject(0).getJSONArray("missedIngredients")
+
+                //seperate all the ingredients
+                seperateIngredients(ingredientsArray)
+
+                //get name of recipe
+
+
+                //get image of recipe
+
+            }
+        }]
+    }
+
+    fun seperateIngredients(ingredientsArray : JSONArray){
+        val ingredientNames = mutableListOf<String>()
+
+        for (i in 0 until ingredientsArray.length()) {
+            val ingredient = ingredientsArray.getJSONObject(i)
+            val name = ingredient.getString("original")
+            ingredientNames.add(name)
+        }
+
+        ingredientStorage.add(ingredientNames)
+    }
+
+    fun getName(recipeArray: JSONArray){
+
+    }
+
+    fun getImage(){
+
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 }
+
